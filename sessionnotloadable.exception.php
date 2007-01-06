727ed002c7447bb4a464a30fdb0baf646c9b02fc
xhochy <xhochy@6ac85236-5c26-0410-b090-ad656868dfd0>
2007-01-06 10:04:16 +0000

    Initial Submission by XhochY

---
+++ sessionnotloadable.exception.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Wird geworfen, wenn eine Session schon benutzt wird.
+ *
+ * @author Uwe L. Korn <uwelk@xhochy.org>
+ */
+class SessionNotLoadableException extends XYException
+{
+    public function __construct($message, $code = 0)
+    {
+        parent::__construct($message, $code);
+    }
+    
+    protected function GetMailSubject()
+    {
+        return 'SessionNotLoadableException in file '.$this->aTrace[0]['File']
+            .' at line '.$this->aTrace[0]['Line'];
+    }
+
+    protected function GetMailBody()
+    {
+        $sResult = 'SessionNotLoadableException in file '.$this->aTrace[0]['File']
+            .' at line '.$this->aTrace[0]['Line']."\n";
+        $sResult.= "\nTrace:\n";
+        $sResult.= $this->TraceToString();
+        $sResult.= "\nVars:\n";
+        $sResult.= $this->VarsToString();
+        return $sResult;
+    }
+}
